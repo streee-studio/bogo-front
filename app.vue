21687a20e89b7a04d13bf42283d5f3c367292885
streee-studio <studio@streee.com>
2024-11-27 13:51:49 +0900

    seo

--- app.vue
+++ app.vue
@@ -3,3 +3,18 @@
         <nuxt-page></nuxt-page>
     </NuxtLayout>
 </template>
+<script setup>
+useHead({
+    link: [
+        { rel: 'icon', type: 'image/png', href: '/favicon.png' }
+    ]
+})
+
+useSeoMeta({
+    title:'bogo - 편의점 행사상품 정보',
+    ogTitle:'bogo - 편의점 행사상품 정보',
+    description:'편의점 1+1, 2+1 행사상품 정보를 한눈에 확인하세요',
+    ogDescription:'편의점 1+1, 2+1 행사상품 정보를 한눈에 확인하세요',
+    ogImage:'/og.jpg'
+})
+</script>
