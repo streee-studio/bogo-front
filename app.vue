6993e19fab8023538dbf2c36c35e318329278f44
streee-studio <studio@streee.com>
2024-11-28 10:03:30 +0900

    upload

--- app.vue
+++ app.vue
@@ -4,6 +4,8 @@
     </NuxtLayout>
 </template>
 <script setup>
+import { nextTick} from "vue";
+
 const config = useRuntimeConfig()
 
 useHead({
@@ -11,50 +13,37 @@ useHead({
         { rel: 'icon', type: 'image/png', href: '/favicon.png' }
     ],
     script: [
-        // {
-        //     src: "https://wcs.naver.net/wcslog.js",
-        //     type: "text/javascript",
-        //     async: true,
-        // }
+        {
+            src: "//wcs.naver.net/wcslog.js",
+            type: "text/javascript",
+            async: true,
+        }
+    ],
+    meta: [
+        {
+            name: 'naver-site-verification',
+            content: '344db43a91ba9a8f26372f58d58095d7559b9deb',
+        },
     ],
 })
 
-
 useSeoMeta({
-    title:'bogo - 편의점 행사상품 정보',
-    ogTitle:'bogo - 편의점 행사상품 정보',
-    description:'편의점 1+1, 2+1 행사상품 정보를 한눈에 확인하세요',
-    ogDescription:'편의점 1+1, 2+1 행사상품 정보를 한눈에 확인하세요',
+    title:'bogo - 편의점 행사상품 검색',
+    ogTitle:'bogo - 편의점 행사상품 검색',
+    description:'보고에서 편의점 1+1, 2+1 행사상품을 한눈에 확인하세요',
+    ogDescription:'보고에서 편의점 1+1, 2+1 행사상품을 한눈에 확인하세요',
     ogImage:config.public.siteURL + '/og.jpg'
 })
 
 if(process.client){
-    const script = document.createElement('script');
-    script.src = 'https://wcs.naver.net/wcslog.js';
-    // script.async = true; // 비동기로 로드할지 결정
-    document.head.appendChild(script);
-
+    // google analytics
     const script2 = document.createElement('script');
     script2.src = 'https://www.googletagmanager.com/gtag/js?id=G-86SX998S27';
-    // script.async = true; // 비동기로 로드할지 결정
     document.head.appendChild(script2);
-
-    if(!wcs_add) var wcs_add = {};
-    wcs_add["wa"] = "13fd646f5b16270";
-    console.log(wcs_add)
-    if(window.wcs) {
-        wcs_do();
-        console.log('wcs do')
-    }else{
-        console.log('no window wcs')
-    }
-
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-
     gtag('config', 'G-86SX998S27');
-    // https://www.googletagmanager.com/gtag/js?id=G-86SX998S27
 }
 
 </script>
